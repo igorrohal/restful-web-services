2fa759ee5b5cc6b9163f38f18c2a8f6f02892ad1
Igor Rohal <igor.rohal@gmail.com>
2020-09-11 09:54:31 +0200

    versioning via URIs, custom header or content negotiation

--- src/main/java/com/irohal/rest/webservices/restfulwebservices/versioning/VersioningController.java
+++ src/main/java/com/irohal/rest/webservices/restfulwebservices/versioning/VersioningController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningController {
 
+    // URI versioning (also with query params another possibility)
     @GetMapping("/v1/person")
     public PersonV1 personV1() {
         return new PersonV1("Bob Charlie");
@@ -13,6 +14,28 @@ public class VersioningController {
 
     @GetMapping("/v2/person")
     public PersonV2 personV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // versioning via custom headers - misuse of what headers were intented for
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 personHeaderV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 personHeaderV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // versioning via content negotiation
+    @GetMapping(value = "/person/produces", produces = "application/vnd.custom.app-v1+json")
+    public PersonV1 personProducesV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.custom.app-v2+json")
+    public PersonV2 personProducesV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
